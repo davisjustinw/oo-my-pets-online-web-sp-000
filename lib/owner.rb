fdbbe5619b1281cdb1f16711cf2b88843775ff31
Justin Davis <davisjustinw@gmail.com>
2019-01-15 04:39:01 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,8 +59,12 @@ class Owner
     end
   end
   
+  def count(species)
+    @pets[species].count
+  end
+  
   def list_pets
-    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+    "I have #{count(:fishes)} fish, #{count(:dogs)} dog(s), and #{count(:cats)} cat(s)."
   end
   
   private
