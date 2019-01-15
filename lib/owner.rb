a0c0245349e9783ae0ecd900b9c3e48051ac80c3
Justin Davis <davisjustinw@gmail.com>
2019-01-15 03:37:26 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,20 +1,22 @@
 class Owner
   attr_accessor :pets
   
+  @@all = []
+  
   
   def initialize
     @pets = {fishes: [], cats: [], dogs: []}
   end
   
   def buy_cat(name)
-    @pets.cats << Cat.new(name)
+    @pets[:cats] << Cat.new(name)
   end
   
   def buy_dog(name)
-    @pets.dogs << Dog.new(name)
+    @pets[:dogs] << Dog.new(name)
   end
   
   def buy_fish(name)
-    @pets.fishes << Fish.new(name)
+    @pets[:fishes] << Fish.new(name)
   end
 end
