5b215e195c9968fabedd2f3533b6cb04ab4b34a3
Justin Davis <davisjustinw@gmail.com>
2019-01-15 03:32:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,15 +1,10 @@
 class Owner
-  attr_accessor :name
+  attr_accessor :pets
   
   def initialize
     @pets = {fishes: [], cats: [], dogs: []}
   end
   
-  def self.count_all
-    @@all.count
-  end
-  
-  def self.reset_all
-    @@all.clear
-  end
+  def buy_cat(name)
+    @pets.cats << Cat.new(name)
 end
